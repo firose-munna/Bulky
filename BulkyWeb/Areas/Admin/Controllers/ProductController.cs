4b3a5edeb9ed76f310e512279d167fe798d64878
firose-munna <munnafirose@gmail.com>
2024-04-03 19:35:18 +0600

    category navigaton property is done

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -13,20 +13,23 @@ namespace BulkyWeb.Areas.Admin.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
 
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
-            List<Product> objCategoryList = _unitOfWork.Product.GetAll().ToList();
+            List<Product> objCategoryList = _unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
 
             
              
             return View(objCategoryList);
         }
 
-        public IActionResult Create()
+        /*public IActionResult Create()
         {
             
 
@@ -40,16 +43,41 @@ namespace BulkyWeb.Areas.Admin.Controllers
                 Product = new Product()
             };
             return View(productVM);
+        }*/
+
+        public IActionResult Upsert(int? id)
+        {
+
+
+            ProductVM productVM = new()
+            {
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Product = new Product()
+            };
+
+            if(id== null || id == 0) {
+                return View(productVM);
+            }
+            else
+            {
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productVM);
+            }
+            
         }
 
-        [HttpPost]
+        /*[HttpPost]
         public IActionResult Create(ProductVM productVM)
         {
 
-            /*if(obj.Name == obj.DisplayOrder.ToString())
+            *//*if(obj.Name == obj.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("name", "The Name cannot exactly match the Display Order.");
-            }*/
+            }*//*
 
 
             if (ModelState.IsValid)
@@ -72,9 +100,73 @@ namespace BulkyWeb.Areas.Admin.Controllers
                 
             }
            
+        }*/
+
+        [HttpPost]
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
+        {
+
+            /*if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Name cannot exactly match the Display Order.");
+            }*/
+
+
+            if (ModelState.IsValid)
+            {
+
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+
+                if(file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    if(!string.IsNullOrEmpty(productVM.Product.ImageUrl)) {
+                        var oldImagePath = Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+
+                        if(System.IO.File.Exists(oldImagePath)) {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    using(var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
+
+                if(productVM.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(productVM.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(productVM.Product);
+                }
+                
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+
+            }
+
+            else
+            {
+                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+                return View(productVM);
+
+            }
+
         }
 
-        public IActionResult Edit(int? id)
+       /* public IActionResult Edit(int? id)
         {
 
             if (id == null || id == 0)
@@ -104,7 +196,7 @@ namespace BulkyWeb.Areas.Admin.Controllers
             }
             return View();
 
-        }
+        }*/
 
         public IActionResult Delete(int? id)
         {
